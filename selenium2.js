9a20176a3595f75144149961b5bdcd9be0ae2cb0
Eyeball Edward <edwardyeung.private@gmail.com>
2020-09-07 21:30:06 +1000

    comment

--- selenium2.js
+++ selenium2.js
@@ -33,45 +33,44 @@ const driver = new webdriver.Builder()
   .setChromeOptions(chromeOptions)
   .build()
 
-driver.get('https://www.onlinemictest.com/').then(async () => { //thomo
-// driver.get('https://meetingsapac21.webex.com/webappng/sites/meetingsapac21/meeting/download/07726931877317ce88086d429d58187c').then(async () => { //thomo
-//   await sleep(4000)
-//   await (await driver.findElement(By.id('push_download_join_by_browser'))).click()
-//   await sleep(1700)
-//   await driver.switchTo().frame(await driver.findElement(By.id('pbui_iframe')))
-//   // await driver.executeScript(injectJS)
-//   await sleep(6000)
-//
-//   let usernameField = await driver.findElement(By.css('[placeholder="Your full name"]'))
-//   let emailField = await driver.findElement(By.css('[placeholder="Email address"]'))
-//   let joinButton = await driver.findElement(By.id('guest_next-btn'))
-//
-//   await usernameField.sendKeys('Ned\'s music bot')
-//   await emailField.sendKeys('ned@transportsg.me')
-//   await joinButton.click()
-//
-//   await sleep(5000)
-//
-//   await (await driver.findElement(By.id('welcome_skip'))).click()
-//   await sleep(500)
-//   await (await driver.findElement(By.id('interstitial_join_btn'))).click()
-//
-//   // await driver.executeScript(injectJS)
-//   //
-//   // for (let i = 0; i < 15; i++) {
-//   //   try {
-//   //     let connectButton = await driver.findElement(By.css('[datadoi="AUDIO:VOIP:MENU_AUDIO"]'))
-//   //     await connectButton.click()
-//   //     break
-//   //   } catch (e) {
-//   //     await sleep(2000)
-//   //   }
-//   // }
-//   //
-//   // await sleep(7000)
-//   //
-//   // for (let i = 0; i < 20; i++)
-//   //   await driver.executeScript(`window.audioQueue.schedulePlay("mii.mp3")`)
-//   //
-//   // await sleep(60 * 1000)
+driver.get('https://meetingsapac21.webex.com/webappng/sites/meetingsapac21/meeting/download/07726931877317ce88086d429d58187c').then(async () => { //thomo
+  await sleep(4000)
+  await (await driver.findElement(By.id('push_download_join_by_browser'))).click()
+  await sleep(1700)
+  await driver.switchTo().frame(await driver.findElement(By.id('pbui_iframe')))
+  // await driver.executeScript(injectJS)
+  await sleep(6000)
+
+  let usernameField = await driver.findElement(By.css('[placeholder="Your full name"]'))
+  let emailField = await driver.findElement(By.css('[placeholder="Email address"]'))
+  let joinButton = await driver.findElement(By.id('guest_next-btn'))
+
+  await usernameField.sendKeys('Ned\'s music bot')
+  await emailField.sendKeys('ned@transportsg.me')
+  await joinButton.click()
+
+  await sleep(5000)
+
+  await (await driver.findElement(By.id('welcome_skip'))).click()
+  await sleep(500)
+  await (await driver.findElement(By.id('interstitial_join_btn'))).click()
+
+  // await driver.executeScript(injectJS)
+  //
+  // for (let i = 0; i < 15; i++) {
+  //   try {
+  //     let connectButton = await driver.findElement(By.css('[datadoi="AUDIO:VOIP:MENU_AUDIO"]'))
+  //     await connectButton.click()
+  //     break
+  //   } catch (e) {
+  //     await sleep(2000)
+  //   }
+  // }
+  //
+  // await sleep(7000)
+  //
+  // for (let i = 0; i < 20; i++)
+  //   await driver.executeScript(`window.audioQueue.schedulePlay("mii.mp3")`)
+  //
+  // await sleep(60 * 1000)
 })
